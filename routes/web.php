f9b8cd24adf63c2d3965985c447f5cd4cc0aa798
Benony Gabriel <gbenn.work@gmail.com>
2024-12-07 18:16:06 +0700

    Add hello view and routes for /coba and /coba/hello

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Membuat route baru dengan nama /coba (localhost:8000/coba)
+Route::get('/coba', function () {
+    return view('coba.hello'); // return view dari (coba/hello.blade.php)
+});
+
+// Membuat route baru dengan nama coba/hello (localhost:8000/coba/hello)
+Route::get('/coba/hello', function () {
+    return 'Hello, World!'; // return string 'Hello, World!'
+});
