98ed9d05b3a688645580d09713c32867c5c0204f
Benony Gabriel <gbenn.work@gmail.com>
2024-12-10 01:28:31 +0700

    Update form action and add CSRF token for todo submission

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Todo\TodoController;
 use App\Http\Controllers\Hello\HelloController;
 
 Route::get('/', [TodoController::class, 'index']); // mengakses method index dari TodoController
+Route::post('/', [TodoController::class, 'store']); // mengakses method index dari TodoController
 
 // Membuat route baru dengan nama /coba (localhost:8000/coba)
 Route::get('/coba', function () {
