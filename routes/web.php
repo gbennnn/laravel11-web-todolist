cab25412cd04b0d8a50e69b5514065d3ee4a37b9
Benony Gabriel <gbenn.work@gmail.com>
2024-12-10 13:47:31 +0700

    Add name to update route for TodoController

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Hello\HelloController;
 
 Route::get('/', [TodoController::class, 'index']); // mengakses method index dari TodoController
 Route::post('/', [TodoController::class, 'store']); // mengakses method index dari TodoController
-Route::put('/update/{id}', [TodoController::class, 'update']);
+Route::put('/update/{id}', [TodoController::class, 'update'])->name('update');
 
 // Membuat route baru dengan nama /coba (localhost:8000/coba)
 Route::get('/coba', function () {
