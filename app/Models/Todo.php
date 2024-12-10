b465f8a7b50d868b7afd6bfd51c78dd7f938e24e
Benony Gabriel <gbenn.work@gmail.com>
2024-12-10 12:52:23 +0700

    Add HasFactory trait and define table and fillable properties in Todo model

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'todo';
+    protected $fillable = ['task', 'is_done'];
 }
